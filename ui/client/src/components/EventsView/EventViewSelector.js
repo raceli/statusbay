eebcd0fe98fd2abc197421a8458fe992cc151173
liorbd <liorbd22@gmail.com>
2020-03-22 12:45:04 +0200

    DEV: [ui] remove use of react-lazylog

--- ui/client/src/components/EventsView/EventViewSelector.js
+++ ui/client/src/components/EventsView/EventViewSelector.js
@@ -1,11 +1,13 @@
-import React, { useMemo } from 'react';
+import React, { useEffect, useMemo, useState } from 'react';
 import TableCell from '@material-ui/core/TableCell';
 import TableRow from '@material-ui/core/TableRow';
 import makeStyles from '@material-ui/core/styles/makeStyles';
 import PropTypes from 'prop-types';
-import TableStateless from '../Table/TableStateless';
-import ContainersLogs from './ContainersLogs';
 import Box from '@material-ui/core/Box';
+import { Dialog } from '@material-ui/core';
+import Link from '@material-ui/core/Link';
+import TableStateless from '../Table/TableStateless';
+import ContainersLogsPopup from './ContainersLogsPopup';
 
 const useStyles = makeStyles((theme) => ({
   container: {
@@ -22,14 +24,32 @@ const useStyles = makeStyles((theme) => ({
     width: 10,
     height: 10,
     backgroundColor: `${theme.palette.error.main}`,
-    borderRadius: `50%`,
-    marginRight: 12
-  }
+    borderRadius: '50%',
+    marginRight: 12,
+  },
+  dialog: {
+    top: '64px !important;',
+  },
 }));
 
 
-const EventsViewSelector = ({ items, selected, onRowClick, deploymentId }) => {
+const EventsViewSelector = ({
+  items, selected, onRowClick, deploymentId,
+}) => {
   const classes = useStyles();
+  const [isOpen, setIsOpen] = useState(false);
+  const handleDialogClose = () => {
+    setIsOpen(false);
+  };
+  useEffect(() => {
+    setIsOpen(false);
+  }, [selected]);
+
+  const handleLogsClick = () => {
+    setTimeout(() => {
+      setIsOpen(true);
+    });
+  };
   const tableConfig = useMemo(() => ({
     row: {
       render: (row, rowIndex) => ({ children }) => (
@@ -48,14 +68,18 @@ const EventsViewSelector = ({ items, selected, onRowClick, deploymentId }) => {
       {
         name: 'Pod',
         header: (name) => <TableCell>{name}</TableCell>,
-        cell: (row) => {return <Box display="flex" alignItems="center"><div className={row.hasError ? classes.marker : undefined}></div> {row.name}</Box>},
+        cell: (row) => (
+          <Box display="flex" alignItems="center">
+            <div className={row.hasError ? classes.marker : undefined} />
+            {' '}
+            {row.name}
+          </Box>
+        ),
       },
       {
         name: 'logs',
         header: (name) => <TableCell>{name}</TableCell>,
-        cell: (row) => {
-          return <ContainersLogs podName={row.name} deploymentId={deploymentId} />
-        },
+        cell: () => <Link onClick={handleLogsClick}>Show</Link>,
       },
       {
         name: 'Status',
@@ -66,7 +90,12 @@ const EventsViewSelector = ({ items, selected, onRowClick, deploymentId }) => {
   }), [selected]);
   return (
     <div className={classes.container}>
-      <TableStateless data={items} config={tableConfig} tableSize="small" stickyHeader={false}/>
+      <TableStateless data={items} config={tableConfig} tableSize="small" stickyHeader={false} />
+      {isOpen && (
+      <Dialog className={classes.dialog} open onClose={handleDialogClose} closeAfterTransition={true} onBackdropClick={handleDialogClose} fullScreen>
+        <ContainersLogsPopup onClose={handleDialogClose} deploymentId={deploymentId} podName={items[selected].name} />
+      </Dialog>
+      )}
     </div>
   );
 };
@@ -78,7 +107,7 @@ EventsViewSelector.propTypes = {
   })),
   selected: PropTypes.number,
   onRowClick: PropTypes.func,
-  deploymentId: PropTypes.string.isRequired
+  deploymentId: PropTypes.string.isRequired,
 };
 
 EventsViewSelector.defaultProps = {
